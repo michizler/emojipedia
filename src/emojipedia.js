ba1f94a3ef9243f86dd57155e5705db3249ab918
Bright Uzosike <michizler.brandforyou@gmail.com>
2024-10-22 16:00:25 +0100

    Create emojipedia.js

---
+++ src/emojipedia.js
@@ -0,0 +1,74 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "😳️",
+    name: "Flushed Face",
+    meaning:
+      "Person is shocked, in an uncomfortable situation or has made a mistake. Expresses shame; something is very embarrassing. Reacts to a compliment or a flattering message that the person did not want to receive.",
+  },
+  {
+    id: 5,
+    emoji: "🥵️",
+    name: "Overheated Face",
+    meaning:
+      "It is so hot, I will get a heat stroke right away! Bright red head, the tongue is hanging from the mouth and drops of sweat are on the forehead. The hottest place in the world lies in Iran: In 2005, the highest temperature of 70.7 °C was measured in the Dasht-e Lut desert.",
+  },
+  {
+    id: 6,
+    emoji: "🥶️",
+    name: "Freezing Face",
+    meaning:
+      "It is so cold, you are about to freeze to death. The smile is almost frozen. Already totally blue in the face from the cold temperature, tortured facial expression. Only a cup of tea, coffee, or the hot tub can help now.",
+  },
+  {
+    id: 7,
+    emoji: "😱️",
+    name: "Face Screaming in Fear",
+    meaning:
+      "Horror-stricken face with two hands, screaming. Is scared stiff, feeling nothing but fear and panic. The creepy emoji can also be used ironically or as a symbol for Halloween.",
+  },
+  {
+    id: 8,
+    emoji: "😥️",
+    name: "Disappointed but Relieved Face",
+    meaning:
+      "Is relieved that something is over, but also disappointed. Has experienced a stressful situation with an unpleasant outcome, for example, a test. Has gotten out of a difficult or delicate situation, but not without bumps and bruises.",
+  },
+  {
+    id: 9,
+    emoji: "😓️",
+    name: "Face with Cold Sweat",
+    meaning:
+      "Stress or worry about a bad experience. Things went different than they should have. The school is stressing or there is still so much to do in the office. You are tired, but still have so much work to do.",
+  },
+  {
+    id: 10,
+    emoji: "🤗️",
+    name: "Hugging Face",
+    meaning:
+      "Smiley with red cheeks is stretching out its hands toward you. Wants to hug you. It's an open and cordial gesture and an expression of warmth and friendliness.",
+  },
+];
+
+export default emojipedia;
